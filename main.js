225d4d458f3d16b6dbe0a49a3c360c4346fd8c95
João Marcelo <108005320+marcelojoaos@users.noreply.github.com>
2024-04-11 17:29:00 -0300

    11-4-2024

--- main.js
+++ main.js
@@ -7,14 +7,27 @@ for (let i = 0; i< botoes.length; i++){
     botoes[i].onclick = function (){
         
         for (let j=0; j<botoes. length; j++){
-             botoes [j].classList.remove("ativo")
+             botoes[j].classList.remove("ativo");
+             textos[j].classList.remove("ativo");
         
-        botoes[i].classList.add("ativo");
-     }
+            }
+
+            botoes[i].classList.add("ativo");
+            textos[i].classList.add("ativo");
         }
     }
 
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+ 
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
 
 
 
+
+
+}
 
