9f9806ba3bca49ec279febc5df73304001823e11
João Marcelo <108005320+marcelojoaos@users.noreply.github.com>
2024-04-25 17:30:32 -0300

    25-4-2024

--- main.js
+++ main.js
@@ -19,8 +19,11 @@ for (let i = 0; i< botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-13T00:00:00");
+const tempoObjetivo3 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-02T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
  
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -31,13 +34,27 @@ function calculaTempo(tempoObjetivo){
     let dias = Math.floor(horas/24);
 
     segundos %= 60;
-    minuto %= 60;
+    minutos %= 60;
     horas %= 24;
 
-    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos" ;
     
 
 
 
 }
 
+
+function atualizaCronometro(){
+     for( let i=0; i<contadores.length; i++){
+     contadores[i].textContent = calculaTempo(tempos[i]);
+
+    }
+}
+
+
+function comecaCronometro(){
+   atualizaCronometro();
+   setInterval(atualizaCronometro, 1000)};
+
+  comecaCronometro();
